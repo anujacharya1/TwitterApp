7ae1cd3515a7bd57cea615507647ff55c3888555
anujacharya <anujacharya1@gmail.com>
2016-02-14 00:15:11 -0800

    toolbar

--- app/src/main/java/com/anuj/twitter/activities/TwitterActivity.java
+++ app/src/main/java/com/anuj/twitter/activities/TwitterActivity.java
@@ -2,6 +2,9 @@ package com.anuj.twitter.activities;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 
 import com.anuj.twitter.R;
 
@@ -11,5 +14,28 @@ public class TwitterActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_twitter);
+
+
+        Toolbar twitterToolBar = (Toolbar) findViewById(R.id.my_toolbar);
+
+        setSupportActionBar(twitterToolBar);
+
+        twitterToolBar.setLogo(R.drawable.twitter_logo);
+        twitterToolBar.setBackgroundColor(getResources().getColor(R.color.blue));
+
+        getSupportActionBar().setDisplayShowTitleEnabled(false);
+
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+        getMenuInflater().inflate(R.menu.menu_twitter, menu);
+        return super.onCreateOptionsMenu(menu);
+
+
+    }
+
+
+
 }
