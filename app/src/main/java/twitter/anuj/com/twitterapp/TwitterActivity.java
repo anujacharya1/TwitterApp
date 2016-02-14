b872681f6446aae53102e6dfec835ef6ddf0c762
anujacharya <anujacharya1@gmail.com>
2016-02-13 22:11:14 -0800

    Initial commit

---
+++ app/src/main/java/twitter/anuj/com/twitterapp/TwitterActivity.java
@@ -0,0 +1,13 @@
+package twitter.anuj.com.twitterapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class TwitterActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_twitter);
+    }
+}
